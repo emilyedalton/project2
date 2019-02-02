d452525a6c09bfc6c188e5645773c579b0087f5c
Emily Dalton <emilydalton@dhcp-10-105-17-25.wireless.northwestern.private>
2019-02-02 10:18:44 -0600

    created front end fields that will correspond to eventual model

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,13 @@
 // Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
+var $movieTitle =$("#movie-title");
+var $movieLink = $("#movie-link");
+var $movieDesc= $("#movie-desc");
+var $movieImg = $("#movie-img");
+var $userReview = $("#user-review");
+var $userName= $("#user-name");
 var $submitBtn = $("#submit");
 var $exampleList = $("#example-list");
+
 
 // The API object contains methods for each kind of request we'll make
 var API = {
