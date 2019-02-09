c32997564bf458a17d6ad4fb55a52edc7c729223
Emily Dalton <emilydalton@dhcp-10-105-17-25.wireless.northwestern.private>
2019-02-09 12:06:19 -0600

    api changes by TIM on emilys computer

--- public/js/index.js
+++ public/js/index.js
@@ -135,4 +135,4 @@ var handleDeleteBtnClick = function() {
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick);
-refreshExamples();
+refreshExamples();  
